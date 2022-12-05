ff297dcb3928c227d5c76a08c6f91dbb43d16e2e
hhz <1536845160@qq.com>
2022-12-05 18:16:48 +0800

    图书管理系统

---
+++ src/main/java/com/itheima/service/BookService.java
@@ -0,0 +1,44 @@
+package com.itheima.service;
+
+import com.itheima.domain.Book;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Transactional
+public interface BookService {
+
+    /**
+     * 保存
+     * @param book
+     * @return
+     */
+    public boolean save(Book book);
+
+    /**
+     * 更改
+     * @param book
+     * @return
+     */
+    public boolean update(Book book);
+
+    /**
+     * 根据id删除
+     * @param id
+     * @return
+     */
+    public boolean delete(Integer id);
+
+    /**
+     * 根据id查询
+     * @param id
+     * @return
+     */
+    public Book getById(Integer id);
+
+    /**
+     * 查询所有
+     * @return
+     */
+    public List<Book> getAll();
+}
---
+++ src/main/java/com/itheima/service/impl/BookServiceImpl.java
@@ -0,0 +1,45 @@
+package com.itheima.service.impl;
+
+import com.itheima.dao.BookDao;
+import com.itheima.domain.Book;
+import com.itheima.service.BookService;
+import com.itheima.dao.BookDao;
+import com.itheima.domain.Book;
+import com.itheima.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BookServiceImpl implements BookService {
+
+    @Autowired
+    private BookDao bookDao;
+
+    public boolean save(Book book) {
+        return bookDao.save(book)>0;
+    }
+
+    public boolean update(Book book) {
+        return bookDao.update(book)>0;
+    }
+
+    public boolean delete(Integer id) {
+        return bookDao.delete(id)>0;
+    }
+
+    public Book getById(Integer id) {
+         //将可能出现的疫情进行包装，转换成自定义异常
+//        try {
+//            int i = 1/0;
+//        }catch (Exception e){
+//            throw new SystemException(Code.SYSTEM_TIMEOUT_ERR,"服务器访问超时，请重试",e);
+//        }
+        return  bookDao.getById(id);
+    }
+
+    public List<Book> getAll() {
+        return bookDao.getAll();
+    }
+}
